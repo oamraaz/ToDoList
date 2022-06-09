7effd4e1d5aa540984e14bb268dae309f211ea4f
oamraaz <90813395+oamraaz@users.noreply.github.com>
2022-06-09 23:01:15 +0600

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/ToDoesController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ToDoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Сортировка по выполненным/невыполненным - 1 метод
+        //private IList<ToDo> _toDoList()
+        //{
+        //    var isAlreadyDone = _context.ToDo.OrderBy(e => e.isDone ? 0 : 1).ToList();
+        //    return isAlreadyDone;
+        //}
+        
+        //Сортировка по выполненным/невыполненным - 2 метод
+
+        //private IList<ToDo> GetToDos()
+        //{
+        //    var todos = _context.ToDo.Where(m => m.isDone == true).ToList();
+        //    return todos;
+        //}
+
+        // GET: ToDoes
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.ToDo.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string sortingTasks)
+        {
+            var tasks = _context.ToDo.AsQueryable();
+            ViewData["TasksProccess"] = String.IsNullOrEmpty(sortingTasks) ? "isDone" : "";
+            switch (sortingTasks)
+            {
+                case "isDone":
+                    tasks = tasks.OrderByDescending(x => x.isDone);
+                    break;
+                default:
+                    tasks = tasks.OrderBy(a => a.isDone);
+                    break;
+            }
+            
+            return View(await tasks.AsNoTracking().ToListAsync());
+        }
+
+        // GET: ToDoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDo = await _context.ToDo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(toDo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDo = await _context.ToDo.FindAsync(id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ToDo toDo)
+        {
+            if (id != toDo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(toDo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ToDoExists(toDo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var toDo = await _context.ToDo
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var toDo = await _context.ToDo.FindAsync(id);
+            _context.ToDo.Remove(toDo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return _context.ToDo.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication2/Data/Migrations/20220609143753_newDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Data.Migrations
+{
+    public partial class newDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isDone = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
